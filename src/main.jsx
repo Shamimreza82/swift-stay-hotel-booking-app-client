d94ef771728cef774fb084808a3a9de9ee35d67e
Shamimreza82 <shamimrezaone@gmail.com>
2023-11-05 02:40:13 +0600

    install tanstak query

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
 import AuthProvider from './AuthProvider/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='font-Poppins'>
     <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
+      </QueryClientProvider>
   </React.StrictMode>,
   </div>
 )
--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,11 +1,42 @@
 
+import { useEffect, useState } from 'react';
 import Navber from '../../components/Navber';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 const Rooms = () => {
+
+    const {data, isLoading, error, refetch} = useQuery({
+        queryKey: ['rooms'], 
+        queryFn: async ()=> {
+           const res = await axios.get('http://localhost:5000/api/v1/rooms')
+           return res
+        }
+    })
+
+    if(isLoading){
+        return <div>Loading........</div>
+    }
+
+ console.log(data.data);
+
+
     return (
         <div>
+            
             <Navber></Navber>
-            All Rooms 
+            <div className='max-w-7xl m-auto grid md:grid-cols-10 mt-5 gap-4 '>
+                <div className='bg-slate-200 col-span-7'>
+                    <div className='grid grid-cols-2 gap-5'>
+                        {
+                            data?.data?.map(rooms => <img src={rooms.roomImages[0]} alt="" />)
+                        }
+                    </div>
+                </div>
+                <div className='col-span-3 bg-slate-200'>
+
+                </div>
+            </div>
         </div>
     );
 };
