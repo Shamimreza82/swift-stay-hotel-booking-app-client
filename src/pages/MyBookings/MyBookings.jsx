033c49ffdb416df90a7cb150f1e212e2257bc75e
Shamimreza82 <shamimrezaone@gmail.com>
2023-11-05 07:47:45 +0600

    added deleted button in my booking page

--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import Navber from "../../components/Navber";
 import useAxiosRoute from "../../Hooks/useAxiosRoute";
 import useAuth from "../../Hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 const MyBookings = () => {
   const axiosSecure = useAxiosRoute();
@@ -9,20 +12,59 @@ const MyBookings = () => {
   const [myBooking, setMybooking] = useState([]);
 
   console.log(myBooking);
-  useEffect(() => {
-    axiosSecure.get(`/bookings?email=${user?.email}`).then((res) => {
-      setMybooking(res.data);
-    });
-  }, [axiosSecure, user?.email]);
 
-  const {_id, name, email, roomType, pricePerNight, availability, roomImages,} = myBooking; 
-  console.log(name);
+  const {data, isLoading, error, refetch} = useQuery({
+    queryKey: ['booking'], 
+    queryFn: async () => {
+        const res = await axiosSecure.get(`/bookings?email=${user?.email}`)
+        return res
+    }
+  })
+
+console.log(data?.data);
+
+//   useEffect(() => {
+//     axiosSecure.get(`/bookings?email=${user?.email}`).then((res) => {
+//       setMybooking(res.data);
+//     });
+
+//   }, [axiosSecure, user?.email]);
+
+  const handelDeletebooking = (_id) => {
+    console.log(_id);
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+            axios.delete(`http://localhost:5000/api/v1/deleteBooking/${_id}`)
+            .then(res => {
+                console.log(res);
+                refetch()
+            })
+
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+
+  }
 
   return (
     <div>
       <Navber></Navber>
       <div className=" max-w-7xl m-auto grid grid-cols-12">
-        <div className="bg-slate-200 col-span-8">
+        <div className="bg-slate-200 col-span-8 mt-5">
           <table className="table">
             {/* head */}
             <thead>
@@ -32,8 +74,8 @@ const MyBookings = () => {
                 <th>Price</th>
                 <th>Delete Booking</th>
               </tr>
-            </thead>
-            {myBooking.map((booking) => (
+            </thead >
+            {data?.data.map((booking) => (
               <tbody key={booking._id}>
                 <tr>
 
@@ -62,7 +104,7 @@ const MyBookings = () => {
                   </td>
                   <td>$  {booking.pricePerNight}</td>
                   <th>
-                    <button className="btn btn-ghost btn-xs">X</button>
+                    <button onClick={()=>handelDeletebooking(booking._id)} className="btn btn-ghost btn-xs">X</button>
                   </th>
                 </tr>
               </tbody>
